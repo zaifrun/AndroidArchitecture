b81647abe98de2e291f6e7c2ed1de3a62ca5ab3d
makn <makn@eaaa.dk>
2018-06-22 10:51:15 +0200

    updated datamodel

--- app/src/main/java/org/example/androidarchitecture/MainActivity.java
+++ app/src/main/java/org/example/androidarchitecture/MainActivity.java
@@ -13,7 +13,6 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
